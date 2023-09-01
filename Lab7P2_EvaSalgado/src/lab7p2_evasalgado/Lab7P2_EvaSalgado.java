960e87c7ef8f637be7114baa0dd219a779cce474
evasalgado <imviarsha@gmail.com>
2023-09-01 13:04:19 -0600

    Creación de proyecto

---
+++ Lab7P2_EvaSalgado/src/lab7p2_evasalgado/Lab7P2_EvaSalgado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab7p2_evasalgado;
+
+/**
+ *
+ * @author evaja
+ */
+public class Lab7P2_EvaSalgado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
